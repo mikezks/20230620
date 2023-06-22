e641ee40bd48fbccac805a9669c41e6f219564db
mikezks <23002661+mikezks@users.noreply.github.com>
2023-06-22 20:53:22 +0200

    feat: service customizing

--- libs/tickets/domain/src/lib/infrastructure/flight.service.ts
+++ libs/tickets/domain/src/lib/infrastructure/flight.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Flight } from '../entities/flight';
 import { DefaultFlightService } from './default-flight.service';
+import { DummyFlightService } from './dummy-flight.service';
 
 @Injectable({
   providedIn: 'root',
-  useClass: DefaultFlightService,
+  useFactory: () => {
+    if (false) {
+      return new DummyFlightService();
+    }
+    return new DefaultFlightService();
+  },
 })
 export abstract class FlightService {
   abstract find(from: string, to: string): Observable<Flight[]>;
